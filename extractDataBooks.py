4e7839c860c80c8f29a5b3eba659a0badddf9de5
ene <car.morelli3@gmail.com>
2020-03-04 08:50:04 +0100

    raw EXTRACTDATABOOKS

--- extractDataBooks.py
+++ extractDataBooks.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 from pyspark.sql import SparkSession
 from pymongo import MongoClient
@@ -22,18 +21,19 @@ if __name__ == '__main__':
     db = client.mydb
 
     #Trasforma da csv a pandas
+
     df_book_tags = pd.read_csv("/home/ene/PycharmProjects/Tesi/book_tags.csv")
-    df_books = pd.read_csv("/home/ene/PycharmProjects/Tesi/books.csv")
-    df_ratings = pd.read_csv("/home/ene/PycharmProjects/Tesi/ratings.csv")
+    #df_books = pd.read_csv("/home/ene/PycharmProjects/Tesi/books.csv")
+    #df_ratings = pd.read_csv("/home/ene/PycharmProjects/Tesi/ratings.csv")
     df_tags = pd.read_csv("/home/ene/PycharmProjects/Tesi/tags.csv")
-    df_toRead = pd.read_csv("/home/ene/PycharmProjects/Tesi/to_read.csv")
+    #df_toRead = pd.read_csv("/home/ene/PycharmProjects/Tesi/to_read.csv")
 
     #sistema campi che danno errori
-    df_books.language_code = df_books.language_code.astype(str)
-    df_books.original_title = df_books.original_title.astype(str)
-    df_books.isbn = df_books.isbn.astype(str)
-    df_books.isbn13 = df_books.isbn13.astype(float)
-    df_books.isbn13 = df_books.isbn13.astype(str)
+#    df_books.language_code = df_books.language_code.astype(str)
+#    df_books.original_title = df_books.original_title.astype(str)
+#    df_books.isbn = df_books.isbn.astype(str)
+#    df_books.isbn13 = df_books.isbn13.astype(float)
+#    df_books.isbn13 = df_books.isbn13.astype(str)
 
     # crea una spark session
     spark = SparkSession.builder.getOrCreate()
@@ -41,28 +41,30 @@ if __name__ == '__main__':
     # converte da pandas a i spark dataframe
 
     spark_df_book_tags = spark.createDataFrame(df_book_tags)
-    spark_df_books = spark.createDataFrame(df_books)
-    spark_df_ratings = spark.createDataFrame(df_ratings)
+    #spark_df_books = spark.createDataFrame(df_books)
+    #spark_df_ratings = spark.createDataFrame(df_ratings)
     spark_df_tags = spark.createDataFrame(df_tags)
-    spark_df_toRead = spark.createDataFrame(df_toRead)
+    #spark_df_toRead = spark.createDataFrame(df_toRead)
 
     # Add the spark data frame to the catalog
 
     spark_df_book_tags.createOrReplaceTempView('book_tags')
-    spark_df_books.createOrReplaceTempView('books')
-    spark_df_ratings.createOrReplaceTempView('ratings')
+    #spark_df_books.createOrReplaceTempView('books')
+    #spark_df_ratings.createOrReplaceTempView('ratings')
     spark_df_tags.createOrReplaceTempView('tags')
-    spark_df_toRead.createOrReplaceTempView('to_read')
+    #spark_df_toRead.createOrReplaceTempView('to_read')
 
-    def topNTags4Books2MongoDB(dataFrameBook,dataFrameTags,maxTagsForBook):
+    def topNTags4Books2MongoDB():
+        from pyspark.sql.functions import rank, col
         maxTagsForBook = 10
         window = Window.partitionBy(spark_df_book_tags['goodreads_book_id']).orderBy(spark_df_book_tags['count'].desc())
-
-        alfa = spark_df_book_tags.select('*', rank().over(window).alias('rank')).filter(col('rank') <= maxTagsForBook).orderBy('goodreads_book_id')
-        alfa = alfa.select("goodreads_book_id",col('tag_id').alias('tag_id_alfa'))
+        alfa = spark_df_book_tags.select('*', rank().over(window).alias('rank')).filter(col('rank') <= maxTagsForBook).orderBy('goodreads_book_id')\
+            .select("goodreads_book_id",col('tag_id').alias('tag_id_alfa'))
         gamma = alfa.join(spark_df_tags,alfa.tag_id_alfa == spark_df_tags.tag_id).select('goodreads_book_id','tag_id','tag_name')
         zeta = gamma.toPandas()
-
         col = db.Books_Tags
         col.insert_many(zeta.to_dict('records'))
-        spark.stop()
+
+
+    topNTags4Books2MongoDB()
+    spark.stop()
